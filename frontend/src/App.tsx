b48b76cd90d0e345b8d3abc233a8ccf804262ff3
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-02-08 15:20:40 -0300

    setup de desenvolvimento criado

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-900 text-white">
+      <h1 className="text-4xl font-bold text-purple-500">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
+export default App
